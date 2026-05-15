6c1f15798103052e9e50059fae1805420f47fdd0
DouglasQuintanillaLinares <00273926@uca.edu.sv>
2026-05-15 11:17:43 -0600

    unomas

---
+++ do while/ejercicio7.cpp
@@ -0,0 +1,27 @@
+//Cuenta Regresiva
+//Solicita un número y muestra una cuenta regresiva hasta 0.
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int n;
+
+
+cout << "CUENTA REGRESIVA" << endl;
+cout << "ingrese un numero" << endl;
+cin >> n;
+
+while (n >= 0){
+    cout << n << endl;
+n--;
+
+
+}
+
+cout << "TERMINO LA CUENTA REGRESIVA" << endl;
+
+
+    return 0;
+}
